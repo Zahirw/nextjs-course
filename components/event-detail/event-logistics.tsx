77728f4643a793c52415f6b961486537e37cfaa7
Zahirw <wakidzahir@gmail.com>
2023-02-16 21:40:43 +0700

    create component event detail and add content event detail page

---
+++ components/event-detail/event-logistics.tsx
@@ -0,0 +1,34 @@
+import AddressIcon from '../icons/AddressIcons';
+import DateIcon from '../icons/DateIcon';
+import LogisticsItem from './logistics-item';
+import classes from './event-logistics.module.css';
+import Image from 'next/image';
+
+function EventLogistics(props: any) {
+  const { date, address, image, imageAlt } = props;
+
+  const humanReadableDate = new Date(date).toLocaleDateString('en-US', {
+    day: 'numeric',
+    month: 'long',
+    year: 'numeric',
+  });
+  const addressText = address.replace(', ', '\n');
+
+  return (
+    <section className={classes.logistics}>
+      <div className={classes.image}>
+        <Image src={`/${image}`} alt={imageAlt} width={200} height={200}/>
+      </div>
+      <ul className={classes.list}>
+        <LogisticsItem icon={DateIcon}>
+          <time>{humanReadableDate}</time>
+        </LogisticsItem>
+        <LogisticsItem icon={AddressIcon}>
+          <address>{addressText}</address>
+        </LogisticsItem>
+      </ul>
+    </section>
+  );
+}
+
+export default EventLogistics;
--- src/pages/events/[eventId].tsx
+++ src/pages/events/[eventId].tsx
@@ -1,11 +1,41 @@
-import React from 'react'
+import { Fragment } from 'react';
+import { useRouter } from 'next/router';
 
+import { getEventById } from 'dummy-data';
+import EventSummary from 'components/event-detail/event-summary';
+import EventLogistics from 'components/event-detail/event-logistics';
+import EventContent from 'components/event-detail/event-content';
+import ErrorAlert from 'components/ui/error-alert/error-alert';
 type Props = {}
 
 const EventDetailPage = (props: Props) => {
-  return (
-    <div>EventDetail Page</div>
-  )
+  const router = useRouter();
+
+  const eventId = router.query.eventId as string;
+  const event = getEventById(eventId);
+
+  if (!event) {
+    return (
+      <ErrorAlert>
+        <p>No event found!</p>
+      </ErrorAlert>
+    );
+  }
+
+   return (
+    <Fragment>
+      <EventSummary title={event.title} />
+      <EventLogistics
+        date={event.date}
+        address={event.location}
+        image={event.image}
+        imageAlt={event.title}
+      />
+      <EventContent>
+        <p>{event.description}</p>
+      </EventContent>
+    </Fragment>
+  );
 }
 
 export default EventDetailPage
